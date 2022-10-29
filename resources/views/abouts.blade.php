2713179eb03a2e67b3249f29b7b0e2a401df4444
SamuraiAgri <good.kotan@gmail.com>
2022-10-29 16:54:47 +0900

    他のページを追加

--- resources/views/abouts.blade.php
+++ resources/views/abouts.blade.php
@@ -1,10 +1,52 @@
-<html>
-    <head>
-        <title>Abouts</title>
-    </head>
-    <body>
-        <h1>現在、準備中です。</h1>
-        <table>
-        </table>
-    </body>
-</html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="ja" xml:lang="ja">
+<head>
+  <meta name="viewport" content="width=device-width" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+  <link rel="stylesheet" href="{{ asset('css/top.css') }}" >
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+  <meta name="description" content=" Durantは、果物をインターネットを利用し全国に販売を行う「SamuraiAgri」の運営を行っています。" />
+  <title>Abouts-株式会社Durant-</title>
+</head>
+
+<body class="drawer-close">
+	<div role="main">
+	<div id="header">
+		<div class="wrap">
+			<div class="main_title"><a href="http://dairyjournal.net/"><p>株式会社 Durant</p></a></div>
+				<ul class="clearfix dp_pc">
+					<li><a href="https://samuraiagri.buyshop.jp/"><p>Cart</p></a></li>
+					<li><a href="/concept"><p>Concept</p></a></li>
+					<li><a href="/wp"><p>Blog</p></a></li>
+					<li><a href="/abouts"><p>Abouts</p></a></li>
+				</ul>
+			</div>
+		</div><!--/#header-->	
+	<div id="container" class="clearfix">
+	<div id="wrapper" class="clearfix">
+		<div class="wrap">
+
+		<div id="">
+			<h2><em>このページは現在準備中です。</em></h2>
+		</div>
+
+		<div id="social_link2">
+		<div class="module_layout common_margin-top__0px">
+		<h2><em>ソーシャルメディア公式アカウント</em></h2>
+			<ul class="social_item2">
+			<li class="social_items2"><a href="https://www.pinterest.jp/" target="_blank"><img src="{{ asset('/img/top/icon/pinterest.png') }}" width="60" height="60" alt="Facebook" class="module_image"></a></li>
+			<li class="social_items2"><a href="https://www.instagram.com/" target="_blank"><img src="{{ asset('/img/top/icon/instagram.png') }}" width="60" height="60" alt="instagram" class="module_image"></a></li>
+			<li class="social_items2"><a href="https://twitter.com/" target="_blank"><img src="{{ asset('/img/top/icon/twitter.png') }}" width="60" height="60" alt="Twitter" class="module_image"></a></li>
+			<li class="social_items2"><a href="https://www.youtube.com/channel/" target="_blank"><img src="{{ asset('/img/top/icon/youtube.png') }}" width="60" height="60" alt="YouTube" class="module_image"></a></li>
+			<li class="social_items2"><a href="" target="_blank"><img src="{{ asset('/img/top/icon/letter.png') }}" width="60" height="60" alt="YouTube" class="module_image"></a></li>
+			</ul>
+		</div>
+		</div>
+		</div><!--/.wrap -->
+	</div><!--/#wrapper-->
+	</div><!--/#container-->
+</body>
+@extends('footer')
+
---
+++ resources/views/footer.blade.php
@@ -0,0 +1,14 @@
+<div id="footer">
+ <div class="wrap clearfix">
+  <ul class="clearfix dp_pc">
+   <li><a href="/public/privacypolicy/">個人情報保護方針</a></li>
+   <li class="sep">&nbsp;</li>
+   <li><a href="mailto:good.kotan@gmail.com">お問い合わせ</a></li>
+  </ul>
+ </div><!--/.wrap-->
+ <p id="copyrights">Copyright &copy; SAMURAI AGRI CO.,LTD All Rights Reserved.</p>
+</div><!-- /role=main -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="{{ asset('/js/top.js') }}"></script>
+</html>
